278a14168747ad801b2e983f72c17e0eeac689aa
wglee <amd0223@gmail.com>
2023-05-19 00:00:54 +0900

    spring servlet project first commit

---
+++ src/main/java/hello/servlet/SpringServletApplication.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/servlet/SpringServletApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
